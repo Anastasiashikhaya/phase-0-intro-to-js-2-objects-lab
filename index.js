d3c4c21c8a75c76dc8a1c0b09b94dce8acb26c16
Anastasia Wakoli <anastasiashikhaya@gmail.com>
2025-03-26 17:09:32 +0300

    Implemented employee objects

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+const employee = {
+    name: "John Doe",
+    streetAddress: "123 Main St"
+};
+
+// 1. updateEmployeeWithKeyAndValue
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        [key]: value
+    };
+}
+
+// 2. destructivelyUpdateEmployeeWithKeyAndValue
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+// 3. deleteFromEmployeeByKey
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+// 4. destructivelyDeleteFromEmployeeByKey
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
